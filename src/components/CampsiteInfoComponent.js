1f544197458bdb01dd65ac734af8ce3ec3db8719
Nathan Jeon <jeon4972@snu.ac.kr>
2021-07-24 22:47:31 +0700

    Workshop

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -7,6 +7,7 @@ import {
   CardBody,
   CardTitle,
 } from "reactstrap";
+import { CAMPSITES } from "../shared/campsites";
 
 class CampsiteInfo extends React.Component {
   constructor(props) {
@@ -30,23 +31,26 @@ class CampsiteInfo extends React.Component {
       return (
         <div className="col-md-5 m-1">
           <h4>Comments</h4>
-          {comments.map(comment => comments.text)}
+          {comments.map(comment =>{
+            return(
+              this.props.comments.text,
+              this.props.comments.date
+              )
+            })}
         </div>
       );
     }
-    
-  }
-  // Inside this pair of curly braces, set up an expression as follows:
-  // Use the array method map on the comments array. You do not need to set up a variable to store the new array. 
-  // See the example in the Code Challenge from this week on constructing lists.
 
-  // {countries.map(country => <div key={country.id}>{country.name}</div>)}
+    return(
+      <div></div>
+    )
+
+  }
 
   // Inside the body of the callback function for the map method, return the comment in two lines 
   // - the first line should show the comment text, and the second line should show the comment author and date. 
   // Use this code to format the date correctly:
   // {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
-  // After the closing curly brace for the if statement, return an empty div.
   
   // Inside the CampsiteInfo component's render() function, under where you called the renderCampsite method, 
   // add the renderComments method, passing in the campsite object's comments array. 
@@ -55,7 +59,8 @@ class CampsiteInfo extends React.Component {
 
   render() {
     if (this.props.campsite) {
-      return <div className="row">{this.renderCampsite(this.props.campsite)}</div>;
+      return <div className="row">{this.renderCampsite(this.props.campsite)}, {this.renderComments(this.props.comment)}</div>;
+      
     } else {
       return <div></div>;
     }
