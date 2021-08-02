889b4b345f3f9a5918f061aa3e2ed657c0b774d4
Nathan Jeon <jeon4972@snu.ac.kr>
2021-08-02 20:09:56 +0700

    Practice

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,29 +1,35 @@
 import React, { Component } from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
-import Directory from "./components/DirectoryComponent";
-import "./App.css";
-import { CAMPSITES } from "./shared/campsites";
+import Directory from "./DirectoryComponent";
+import { CAMPSITES } from "../shared/campsites";
 
-class App extends Component {
+class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       campsites: CAMPSITES,
+      selectedCampsite: null,
     };
+  }
+  onCampsiteSelect(campsiteID) {
+    this.setState({ selectedCampsite: campsiteId });
   }
   render() {
     return (
-      <div className="App">
+      <div>
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">NuCamp</NavbarBrand>
           </div>
         </Navbar>
-        <Directory campsites={this.state.campsites} />
-        {this.props.testprop}
+        <Directory
+          campsites={this.state.campsites}
+          onClick={(campsiteId) => this.onCampsiteSelect(campsite)}
+        />
+        <CampsiteInfo campsite={this.state.selectedCampsite} />
       </div>
     );
   }
 }
 
-export default App;
+export default Main;
