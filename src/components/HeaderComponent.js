8fc0ee4fa4a186c1cfff81fcba608a6797c44a4a
Nathan Jeon <jeon4972@snu.ac.kr>
2021-08-07 03:07:36 +0700

    Practice

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,7 +1,29 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand, Jumbotron } from "reactstrap";
+import {
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Collapse,
+  NavItem,
+  Jumbotron,
+} from "reactstrap";
+import { NavLink } from "react-router-dom";
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleNav = this.toggleNav.bind(this);
+    this.state = {
+      isNavOpen: false,
+    };
+  }
+  toggleNav() {
+    this.setState({
+      isNavOpen: !this.state.isNavOpen,
+    });
+  }
   render() {
     return (
       <React.Fragment>
@@ -15,9 +37,45 @@ class Header extends Component {
             </div>
           </div>
         </Jumbotron>
-        <Navbar dark sticky="top">
+        <Navbar dark sticky="top" expand="md">
           <div className="container">
-            <NavbarBrand href="/">NuCamp</NavbarBrand>
+            <NavbarBrand className="mr-auto" href="/">
+              <img
+                src="/assets/images/logo.png"
+                height="30"
+                width="30"
+                alt="Nucamp Logo"
+              />
+              <NavbarToggler onClick={this.toggleNav} />
+              <Collapse isOpen={this.state.isNavOpen} navbar>
+                <Nav navbar>
+                  <NavItem>
+                    <NavLink className="nav-link" to="/home">
+                      <i className="fa fa-home fa-lg mr-2" />
+                      Home
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink className="nav-link" to="/directory">
+                      <i className="fa fa-list fa-lg mr-2 ml-3" />
+                      directory
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink className="nav-link" to="/aboutus">
+                      <i className="fa fa-info fa-lg mr-2 ml-3" />
+                      About
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink className="nav-link" to="/contactus">
+                      <i className="fa fa-address-card fa-lg mr-2 ml-3" />
+                      Contact Us
+                    </NavLink>
+                  </NavItem>
+                </Nav>
+              </Collapse>
+            </NavbarBrand>
           </div>
         </Navbar>
       </React.Fragment>
