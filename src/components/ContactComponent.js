0d9fc611280c3bfb50078274b1a53234004231e6
Nathan Jeon <jeon4972@snu.ac.kr>
2021-08-09 18:01:58 -0400

    Practice

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,284 +1,94 @@
-import React, { Component } from "react";
+import React from "react";
 import {
   Breadcrumb,
   BreadcrumbItem,
-  Button,
-  Label,
-  Col,
-  Row,
+  Card,
+  CardBody,
+  CardHeader,
+  Media,
 } from "reactstrap";
 import { Link } from "react-router-dom";
-import { Control, LocalForm, Errors } from "react-redux-form";
 
-const required = (val) => val && val.length;
-const maxLength = (len) => (val) => !val || val.length <= len;
-const minLength = (len) => (val) => val && val.length >= len;
-const isNumber = (val) => !isNaN(+val);
-const validEmail = (val) =>
-  /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
-class Contact extends Component {
-  constructor(props) {
-    super(props);
+function About(props) {
+  const partners = props.partners.map((partner) => {
+    return <h5>{partner.name}</h5>;
+  });
 
-    this.state = {
-      firstName: "",
-      lastName: "",
-      phoneNum: "",
-      email: "",
-      agree: false,
-      contactType: "By Phone",
-      feedback: "",
-      touched: {
-        firstName: false,
-        lastName: false,
-        phoneNum: false,
-        email: false,
-      },
-    };
-
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleSubmit(event) {
-    console.log("Current state is: " + JSON.stringify(this.state));
-    alert("Current state is: " + JSON.stringify(this.state));
-    event.preventDefault();
-  }
-
-  handleInputChange(event) {
-    const target = event.target;
-    const name = target.name;
-    const value = target.type === "checkbox" ? target.checked : target.value;
-
-    this.setState({
-      [name]: value,
-    });
-  }
-
-  handleSubmit(values) {
-    console.log("Current state is: " + JSON.stringify(this.state));
-    alert("Current state is: " + JSON.stringify(this.state));
-    event.preventDefault();
-  }
-  render() {
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <Breadcrumb>
-              <BreadcrumbItem>
-                <Link to="/home">Home</Link>
-              </BreadcrumbItem>
-              <BreadcrumbItem active>Contact Us</BreadcrumbItem>
-            </Breadcrumb>
-            <h2>Contact Us</h2>
-            <hr />
-          </div>
-        </div>
-
-        <div className="row row-content align-items-center">
-          <div className="col-sm-4">
-            <h5>Our Address</h5>
-            <address>
-              1 Nucamp Way
-              <br />
-              Seattle, WA 98001
-              <br />
-              U.S.A.
-            </address>
-          </div>
-          <div className="col">
-            <a role="button" className="btn btn-link" href="tel:+12065551234">
-              <i className="fa fa-phone" /> 1-206-555-1234
-            </a>
-            <br />
-            <a
-              role="button"
-              className="btn btn-link"
-              href="mailto:fakeemail@fakeemail.co"
-            >
-              <i className="fa fa-envelope-o" /> campsites@nucamp.co
-            </a>
-          </div>
-        </div>
-        <div className="row row-content">
-          <div className="col-12">
-            <h2>Send us your Feedback</h2>
-            <hr />
-          </div>
-          <div className="col-md-10">
-            <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
-              <Row className="form-group">
-                <Label htmlFor="firstName" md={2}>
-                  First Name
-                </Label>
-                <Col md={10}>
-                  <Control.text
-                    model=".firstName"
-                    id="firstName"
-                    name="firstName"
-                    placeholder="First Name"
-                    className="form-control"
-                    validators={{
-                      required,
-                      minLength: minLength(2),
-                      maxLength: maxLength(15),
-                    }}
-                  />
-                  <Errors
-                    className="text-danger"
-                    model=".firstName"
-                    show="touched"
-                    component="div"
-                    messages={{
-                      required: "Required",
-                      minLength: "Must be at least 2 characters",
-                      maxLength: "Must be 15 characters or less",
-                    }}
-                  />
-                </Col>
-              </Row>
-              <Row className="form-group">
-                <Label htmlFor="lastName" md={2}>
-                  Last Name
-                </Label>
-                <Col md={10}>
-                  <Control.text
-                    model=".lastName"
-                    id="lastName"
-                    name="lastName"
-                    placeholder="Last Name"
-                    className="form-control"
-                    validators={{
-                      required,
-                      minLength: minLength(2),
-                      maxLength: maxLength(15),
-                    }}
-                  />
-                  <Errors
-                    className="text-danger"
-                    model=".lastName"
-                    show="touched"
-                    component="div"
-                    messages={{
-                      required: "Required",
-                      minLength: "Must be at least 2 characters",
-                      maxLength: "Must be 15 characters or less",
-                    }}
-                  />
-                </Col>
-              </Row>
-              <Row className="form-group">
-                <Label htmlFor="phoneNum" md={2}>
-                  Phone
-                </Label>
-                <Col md={10}>
-                  <Control.text
-                    model=".phoneNum"
-                    id="phoneNum"
-                    name="phoneNum"
-                    placeholder="Phone number"
-                    className="form-control"
-                    validators={{
-                      required,
-                      minLength: minLength(10),
-                      maxLength: maxLength(15),
-                      isNumber,
-                    }}
-                  />
-                  <Errors
-                    className="text-danger"
-                    model=".phoneNum"
-                    show="touched"
-                    component="div"
-                    messages={{
-                      required: "Required",
-                      minLength: "Must be at least 10 numbers",
-                      maxLength: "Must be 15 numbers or less",
-                      isNumber: "Must be a number",
-                    }}
-                  />
-                </Col>
-              </Row>
-              <Row className="form-group">
-                <Label htmlFor="email" md={2}>
-                  Email
-                </Label>
-                <Col md={10}>
-                  <Control.text
-                    model=".email"
-                    id="email"
-                    name="email"
-                    placeholder="Email"
-                    className="form-control"
-                    validators={{
-                      required,
-                      validEmail,
-                    }}
-                  />
-                  <Errors
-                    className="text-danger"
-                    model=".email"
-                    show="touched"
-                    component="div"
-                    messages={{
-                      required: "Required",
-                      validEmail: "Invalid email address",
-                    }}
-                  />
-                </Col>
-              </Row>
-              <Row className="form-group">
-                <Col md={{ size: 4, offset: 2 }}>
-                  <div className="form-check">
-                    <Label check>
-                      <Control.checkbox
-                        model=".agree"
-                        name="agree"
-                        className="form-check-input"
-                      />{" "}
-                      <strong>May we contact you?</strong>
-                    </Label>
-                  </div>
-                </Col>
-                <Col md={4}>
-                  <Control.select
-                    model=".contactType"
-                    name="contactType"
-                    className="form-control"
-                  >
-                    <option>By Phone</option>
-                    <option>By Email</option>
-                  </Control.select>
-                </Col>
-              </Row>
-              <Row className="form-group">
-                <Label htmlFor="feedback" md={2}>
-                  Your Feedback
-                </Label>
-                <Col md={10}>
-                  <Control.textarea
-                    model=".feedback"
-                    id="feedback"
-                    name="feedback"
-                    rows="12"
-                    className="form-control"
-                  />
-                </Col>
-              </Row>
-              <Row className="form-group">
-                <Col md={{ size: 10, offset: 2 }}>
-                  <Button type="submit" color="primary">
-                    Send Feedback
-                  </Button>
-                </Col>
-              </Row>
-            </LocalForm>
-          </div>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link to="/home">Home</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>About Us</BreadcrumbItem>
+          </Breadcrumb>
+          <h2>About Us</h2>
+          <hr />
         </div>
       </div>
-    );
-  }
+      <div className="row row-content">
+        <div className="col-sm-6">
+          <h3>Our Mission</h3>
+          <p>
+            We present a curated database of the best campsites in the vast
+            woods and back country of the World Wide Web Wilderness. We increase
+            access to adventure for the public while promoting safe and
+            respectful use of resources. The expert wilderness trekkers on our
+            staff personally verify each campsite to make sure that they are up
+            to our standards. We also present a platform for campers to share
+            reviews on campsites they have visited with each other.
+          </p>
+        </div>
+        <div className="col-sm-6">
+          <Card>
+            <CardHeader className="bg-primary text-white">
+              <h3>Facts At a Glance</h3>
+            </CardHeader>
+            <CardBody>
+              <dl className="row">
+                <dt className="col-6">Founded</dt>
+                <dd className="col-6">February 3, 2016</dd>
+                <dt className="col-6">No. of Campsites in 2019</dt>
+                <dd className="col-6">563</dd>
+                <dt className="col-6">No. of Reviews in 2019</dt>
+                <dd className="col-6">4388</dd>
+                <dt className="col-6">Employees</dt>
+                <dd className="col-6">42</dd>
+              </dl>
+            </CardBody>
+          </Card>
+        </div>
+        <div className="col">
+          <Card className="bg-light mt-3">
+            <CardBody>
+              <blockquote className="blockquote">
+                <p className="mb-0">
+                  I will not follow where the path may lead, but I will go where
+                  there is no path, and I will leave a trail.
+                </p>
+                <footer className="blockquote-footer">
+                  Muriel Strode,{" "}
+                  <cite title="Source Title">
+                    "Wind-Wafted Wild Flowers" - The Open Court, 1903
+                  </cite>
+                </footer>
+              </blockquote>
+            </CardBody>
+          </Card>
+        </div>
+      </div>
+      <div className="row row-content">
+        <div className="col-12">
+          <h3>Community Partners</h3>
+        </div>
+        <div className="col mt-4">
+          <Media list>{partners}</Media>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Contact;
+export default About;
