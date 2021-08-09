646199b9a5cd39f4f120cde140a42702994cf6fc
Nathan Jeon <jeon4972@snu.ac.kr>
2021-08-09 18:32:00 -0400

    Practice

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,94 +1,231 @@
-import React from "react";
+import React, { Component } from "react";
 import {
   Breadcrumb,
   BreadcrumbItem,
-  Card,
-  CardBody,
-  CardHeader,
-  Media,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Col,
+  FormFeedback,
 } from "reactstrap";
+
 import { Link } from "react-router-dom";
 
-function About(props) {
-  const partners = props.partners.map((partner) => {
-    return <h5>{partner.name}</h5>;
-  });
+class Contact extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col">
-          <Breadcrumb>
-            <BreadcrumbItem>
-              <Link to="/home">Home</Link>
-            </BreadcrumbItem>
-            <BreadcrumbItem active>About Us</BreadcrumbItem>
-          </Breadcrumb>
-          <h2>About Us</h2>
-          <hr />
+    this.state = {
+      firstName: "",
+      lastName: "",
+      phoneNum: "",
+      email: "",
+      agree: false,
+      contactType: "By Phone",
+      feedback: "",
+      touched: {
+        firstName: false,
+        lastName: false,
+        phoneNum: false,
+        email: false,
+      },
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  validate(firstName, lastName, phoneNum, email) {
+    const errors = {
+      firstName: "",
+      lastName: "",
+      phoneNum: "",
+      email: "",
+    };
+
+    if (this.state.touched.firstName) {
+      if (firstName.length < 2) {
+        errors.firstName = "First name must be at least 2 characters.";
+      } else if (firstName.length > 15) {
+        errors.firstName = "First name must be 15 or less characters.";
+      }
+    }
+
+    if (this.state.touched.lastName) {
+      if (lastName.length < 2) {
+        errors.lastName = "Last name must be at least 2 characters.";
+      } else if (lastName.length > 15) {
+        errors.lastName = "Last name must be 15 or less characters.";
+      }
+    }
+
+    const reg = /^\d+$/;
+    if (this.state.touched.phoneNum && !reg.test(phoneNum)) {
+      errors.phoneNum = "The phone number should contain only numbers.";
+    }
+
+    if (this.state.touched.email && !email.includes("@")) {
+      errors.email = "Email should contain a @";
+    }
+
+    return errors;
+  }
+
+  handleBlur = (field) => () => {
+    this.setState({
+      touched: { ...this.state.touched, [field]: true },
+    });
+  };
+
+  handleInputChange(event) {
+    const target = event.target;
+    const name = target.name;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleSubmit(event) {
+    console.log("Current state is: " + JSON.stringify(this.state));
+    alert("Current state is: " + JSON.stringify(this.state));
+    event.preventDefault();
+  }
+  render() {
+    const errors = this.validate(
+      this.state.firstName,
+      this.state.lastName,
+      this.state.phoneNum,
+      this.state.email
+    );
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <Breadcrumb>
+              <BreadcrumbItem>
+                <Link to="/home">Home</Link>
+              </BreadcrumbItem>
+              <BreadcrumbItem active>Contact Us</BreadcrumbItem>
+            </Breadcrumb>
+            <h2>Contact Us</h2>
+            <hr />
+          </div>
+        </div>
+
+        <div className="row row-content align-items-center">
+          <div className="col-sm-4">
+            <h5>Our Address</h5>
+            <address>
+              1 Nucamp Way
+              <br />
+              Seattle, WA 98001
+              <br />
+              U.S.A.
+            </address>
+          </div>
+          <div className="col">
+            <a role="button" className="btn btn-link" href="tel:+12065551234">
+              <i className="fa fa-phone" /> 1-206-555-1234
+            </a>
+            <br />
+            <a
+              role="button"
+              className="btn btn-link"
+              href="mailto:fakeemail@fakeemail.co"
+            >
+              <i className="fa fa-envelope-o" /> campsites@nucamp.co
+            </a>
+          </div>
+        </div>
+        <div className="row row-content">
+          <div className="col-12">
+            <h2>Send us your Feedback</h2>
+            <hr />
+          </div>
+          <div className="col-md-10">
+            <Form onSubmit={this.handleSubmit}>
+              <FormGroup row>
+                <Label htmlFor="firstName" md={2}>
+                  First Name
+                </Label>
+                <Col md={10}>
+                  <Input
+                    type="text"
+                    id="firstName"
+                    name="firstName"
+                    placeholder="First Name"
+                    value={this.state.firstName}
+                    invalid={errors.firstName}
+                    onBlur={this.handleBlur("firstName")}
+                    onChange={this.handleInputChange}
+                  />
+                  <FormFeedback>{errors.firstName}</FormFeedback>
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label htmlFor="lastName" md={2}>
+                  Last Name
+                </Label>
+                <Col md={10}>
+                  <Input
+                    type="text"
+                    id="lastName"
+                    name="lastName"
+                    placeholder="Last Name"
+                    value={this.state.lastName}
+                    invalid={errors.lastName}
+                    onBlur={this.handleBlur("lastName")}
+                    onChange={this.handleInputChange}
+                  />
+                  <FormFeedback>{errors.lastName}</FormFeedback>
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label htmlFor="phoneNum" md={2}>
+                  Phone
+                </Label>
+                <Col md={10}>
+                  <Input
+                    type="tel"
+                    id="phoneNum"
+                    name="phoneNum"
+                    placeholder="Phone number"
+                    value={this.state.phoneNum}
+                    invalid={errors.phoneNum}
+                    onBlur={this.handleBlur("phoneNum")}
+                    onChange={this.handleInputChange}
+                  />
+                  <FormFeedback>{errors.phoneNum}</FormFeedback>
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label htmlFor="email" md={2}>
+                  Email
+                </Label>
+                <Col md={10}>
+                  <Input
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="Email"
+                    value={this.state.email}
+                    invalid={errors.email}
+                    onBlur={this.handleBlur("email")}
+                    onChange={this.handleInputChange}
+                  />
+                  <FormFeedback>{errors.email}</FormFeedback>
+                </Col>
+              </FormGroup>
+            </Form>
+          </div>
         </div>
       </div>
-      <div className="row row-content">
-        <div className="col-sm-6">
-          <h3>Our Mission</h3>
-          <p>
-            We present a curated database of the best campsites in the vast
-            woods and back country of the World Wide Web Wilderness. We increase
-            access to adventure for the public while promoting safe and
-            respectful use of resources. The expert wilderness trekkers on our
-            staff personally verify each campsite to make sure that they are up
-            to our standards. We also present a platform for campers to share
-            reviews on campsites they have visited with each other.
-          </p>
-        </div>
-        <div className="col-sm-6">
-          <Card>
-            <CardHeader className="bg-primary text-white">
-              <h3>Facts At a Glance</h3>
-            </CardHeader>
-            <CardBody>
-              <dl className="row">
-                <dt className="col-6">Founded</dt>
-                <dd className="col-6">February 3, 2016</dd>
-                <dt className="col-6">No. of Campsites in 2019</dt>
-                <dd className="col-6">563</dd>
-                <dt className="col-6">No. of Reviews in 2019</dt>
-                <dd className="col-6">4388</dd>
-                <dt className="col-6">Employees</dt>
-                <dd className="col-6">42</dd>
-              </dl>
-            </CardBody>
-          </Card>
-        </div>
-        <div className="col">
-          <Card className="bg-light mt-3">
-            <CardBody>
-              <blockquote className="blockquote">
-                <p className="mb-0">
-                  I will not follow where the path may lead, but I will go where
-                  there is no path, and I will leave a trail.
-                </p>
-                <footer className="blockquote-footer">
-                  Muriel Strode,{" "}
-                  <cite title="Source Title">
-                    "Wind-Wafted Wild Flowers" - The Open Court, 1903
-                  </cite>
-                </footer>
-              </blockquote>
-            </CardBody>
-          </Card>
-        </div>
-      </div>
-      <div className="row row-content">
-        <div className="col-12">
-          <h3>Community Partners</h3>
-        </div>
-        <div className="col mt-4">
-          <Media list>{partners}</Media>
-        </div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
-export default About;
+export default Contact;
