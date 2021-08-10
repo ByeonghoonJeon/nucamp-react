53ee702f878a1c9499d3931bb27997e26b77f0c5
Nathan Jeon <jeon4972@snu.ac.kr>
2021-08-10 14:48:28 -0400

    Workshop Done

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -24,6 +24,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPencilAlt } from "@fortawesome/free-solid-svg-icons";
 import { Control, LocalForm, Errors } from "react-redux-form";
 
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => val && val.length >= len;
+
 function RenderCampsite({ campsite }) {
   return (
     <div className="col-md-5 m-1">
@@ -105,8 +109,23 @@ class CommentForm extends React.Component {
     this.toggleNav = this.toggleNav.bind(this);
     this.toggleModal = this.toggleModal.bind(this);
     this.submitComment = this.submitComment.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
+  validate(author) {
+    const errors = {
+      author: "",
+    };
+
+    if (this.state.touched.author) {
+      if (author.length < 2) {
+        errors.author = "First name must be at least 2 characters.";
+      } else if (author.length > 15) {
+        errors.author = "First name must be 15 or less characters.";
+      }
+    }
+    return errors;
+  }
   toggleNav() {
     this.setState({
       isNavOpen: !this.state.isNavOpen,
@@ -119,12 +138,27 @@ class CommentForm extends React.Component {
     });
   }
 
+  handleInputChange(event) {
+    this.setState(event);
+    // console.log("ok", event);
+    // const target = event.target;
+    // const name = target.name;
+    // const value = target.type === "checkbox" ? target.checked : target.value;
+
+    // this.setState({
+    //   [name]: value,
+    // });
+  }
+
   submitComment(event) {
-    alert(
-      `Rating: ${this.rating.value} Your Name: ${this.yourname.value} Comment: ${this.comment.value}`
-    );
+    const { rating, author, text } = this.state;
+    const formState = {
+      rating,
+      author,
+      text,
+    };
+    alert(`Current State is: ${JSON.stringify(formState)}`);
     this.toggleModal();
-    event.preventDefault();
   }
 
   render() {
@@ -136,31 +170,61 @@ class CommentForm extends React.Component {
         <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
           <ModalHeader toggle={this.toggleModal}>Submit comment</ModalHeader>
           <ModalBody>
-            <LocalForm onSubmit={this.submitComment}>
-              <FormGroup>
+            <LocalForm
+              onSubmit={this.submitComment}
+              onChange={this.handleInputChange}
+            >
+              <div className="form-group">
                 <Label for="rating">Rating</Label>
-                <Col sm={10}>
-                  <Control.select model=".rating" id="rating">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                  </Control.select>
-                </Col>
-              </FormGroup>
-              <FormGroup>
-                <Label htmlFor="yourname">Your Name</Label>
-                <Input
-                  type="yourname"
-                  id="yourname"
-                  name="yourname"
-                  innerRef={(input) => (this.yourname = input)}
+                <Control.select
+                  model=".rating"
+                  name="rating"
+                  id="rating"
+                  className="form-control"
+                >
+                  <option value="5">5</option>
+                  <option value="4">4</option>
+                  <option value="3">3</option>
+                  <option value="2">2</option>
+                  <option value="1">1</option>
+                </Control.select>
+              </div>
+              <div className="form-group">
+                <Label htmlFor="author">Your Name</Label>
+                <Control.text
+                  model=".author"
+                  id="author"
+                  name="author"
+                  className="form-control"
+                  placeholder="Your name"
+                  validators={{
+                    required,
+                    minLength: minLength(2),
+                    maxLength: maxLength(15),
+                  }}
                 />
-              </FormGroup>
+                <Errors
+                  className="text-danger"
+                  model=".author"
+                  show="touched"
+                  component="div"
+                  messages={{
+                    required: "Required",
+                    minLength: "Must be at least 2 characters",
+                    maxLength: "Must be 15 characters or less",
+                  }}
+                />
+              </div>
               <FormGroup>
-                <Label for="comment">Comment</Label>
-                <Input type="textarea" name="comment" id="comment" />
+                <Label htmlFor="text">Comment</Label>
+                <Control.textarea
+                  model=".text"
+                  name="text"
+                  id="text"
+                  className="form-control"
+                  rows={6}
+                  placeholder="Comment here"
+                />
               </FormGroup>
               <Button type="submit" value="submit" color="primary">
                 Submit
